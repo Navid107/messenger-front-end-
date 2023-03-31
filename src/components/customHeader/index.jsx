43277990695bcbfa680b471478bc4a9aefb8bdd3
Navid Yazdani <mohammadYazdani7@gmail.com>
2023-03-30 22:50:31 -0700

    login page

--- src/components/customHeader/index.jsx
+++ src/components/customHeader/index.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import { ChatBubbleLeftRightIcon, PhoneIcon } from '@heroicons/react/24/solid';
+import React from "react";
+import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid";
 
-const CustomerHeader = ({ chat }) => {    
+const CustomerHeader = ({ chat }) => {
   return (
-    <div>
-        <div className='chat-header'>
-            <div className='flexbetween'>
-                <ChatBubbleLeftRightIcon className='icon-chat' />
-                <h3 className='header-text'>{chat.title}</h3>
-            </div>
-            <div className='flexbetween'>
-                <PhoneIcon className='icon-phone' />
-               {chat.description !== "⬅️ ⬅️ ⬅️" ? 
-               (<p className='header-text'>{chat.description}</p>) :
-               (<p className='header-text'>No chat selected</p>)
-            }
-            </div>
-        </div>
+    <div className="chat-header">
+      <div className="flexbetween">
+        <ChatBubbleLeftRightIcon className="icon-chat" />
+        <h3 className="header-text">{chat.title}</h3>
+      </div>
+      <div className="flexbetween">
+        <PhoneIcon className="icon-phone" />
+        {chat.description !== "⬅️ ⬅️ ⬅️" ? (
+          <p className="header-text">{chat.description}</p>
+        ) : (
+          <p className="header-text">no chat selected</p>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CustomerHeader
+export default CustomerHeader;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.scss";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
@@ -13,9 +13,8 @@ export const store = configureStore({
 });
 setupListeners(store.dispatch);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-    </Provider>
-)
+  </Provider>
+);
