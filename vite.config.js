72b4ddabc3df7737c479640e70489524a944a800
C. Roberts <90425833+Navid107@users.noreply.github.com>
2023-04-03 19:38:28 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,20 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src')}]
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 })
