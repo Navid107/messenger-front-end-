820916ea0abdea6315cdcb98a640a0da29a212bf
C. Roberts <90425833+Navid107@users.noreply.github.com>
2023-04-04 22:36:45 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,18 +9,13 @@ export default defineConfig({
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src')}]
   },
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
+    chunkSizeWarningLimit: 1500,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      exclude: [
+        "node_modules/lodash-es/**",
+        "node_modules/@types/lodash-es/**",
+      ],
     },
   },
 })
